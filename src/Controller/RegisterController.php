1ef4568e9ffb7818603a3fb2f6925450a6c25625
KENPACHI <aaron.homassel@gmail.com>
2023-01-16 21:17:40 +0100

    modified:   src/Controller/RegisterController.php

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class RegisterController extends AbstractController
 {
@@ -17,25 +19,29 @@ class RegisterController extends AbstractController
     public function __construct(EntityManagerInterface $entityManager){
 
         $this->entityManager = $entityManager;
-    
 
 
     }
         
-
-
-
-
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
+       
+        
         $form = $this->createForm(RegisterType::class, $user);
 
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
+
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+
+$user->setPassword($password);
+
+
+
             $this->entityManager->persist($user);
             $this->entityManager->flush();
             
