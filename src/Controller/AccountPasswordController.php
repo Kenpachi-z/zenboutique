7276989c7f3d7bd643bd8565207e3a19d514ada9
KENPACHI <aaron.homassel@gmail.com>
2023-01-20 15:22:00 +0100

    modified:   src/Controller/AccountPasswordController.php modified:   templates/account/password.html.twig

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,11 @@ class AccountPasswordController extends AbstractController
     #[Route('/compte/password', name: 'app_account_password')]
     public function index(): Response
     {
-        return $this->render('account/password.html.twig');
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        return $this->render('account/password.html.twig',[
+        'form'=>$form->createView()
+        ] );
     }
 }
