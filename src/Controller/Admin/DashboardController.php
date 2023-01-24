895891a2c05f66f2f7f49ba5a0b9186e7e0b00e4
KENPACHI <aaron.homassel@gmail.com>
2023-01-24 16:34:09 +0100

    modified:   .gitignore new file:   assets/app.js 	new file:   assets/bootstrap.js 	new file:   assets/controllers.json 	new file:   assets/controllers/hello_controller.js 	new file:   assets/styles/app.css 	modified:   composer.json 	modified:   composer.lock 	modified:   config/bundles.php 	new file:   config/packages/webpack_encore.yaml new file:   migrations/Version20230124132748.php new file:   migrations/Version20230124145629.php new file:   package.json 	new file:   src/Controller/Admin/CategoryCrudController.php modified:   src/Controller/Admin/DashboardController.php 	new file:   src/Controller/Admin/ProductCrudController.php 	new file:   src/Entity/Category.php 	new file:   src/Entity/Product.php 	new file:   src/Repository/CategoryRepository.php 	new file:   src/Repository/ProductRepository.php 	modified:   symfony.lock 	new file:   webpack.config.js

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,24 +2,36 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+public function __construct(private AdminUrlGenerator  $adminUrlGenerator){
+
+}
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+       // return parent::index();
+
+        $url = $this->adminUrlGenerator->setController(UserCrudController::class)
+            ->generateUrl();
+        return $this->redirect($url);
+        
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -42,6 +54,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-         yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
+         yield MenuItem::linkToCrud('Utilisateur', 'fa fa-user', User::class);
+         yield MenuItem::linkToCrud('Catégorie', 'fa fa-list', Category::class);
+         yield MenuItem::linkToCrud('Produit', 'fa fa-tags', Product::class);
     }
 }
