94daefceb7f0c2843896baf99153b2ce7dc2726a
KENPACHI <aaron.homassel@gmail.com>
2023-01-24 13:09:21 +0100

    modified:   src/Controller/Admin/DashboardController.php modified:   templates/base.html.twig

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -42,6 +42,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-         yield MenuItem::linkToCrud('The Label', 'fas fa-user', User::class);
+         yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
     }
 }
