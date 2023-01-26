44e15deac83b7c2284a44f4b8bea99f92c1852e1
KENPACHI <aaron.homassel@gmail.com>
2023-01-26 22:12:59 +0100

    modified:   src/Controller/ProduitController.php 	modified:   templates/base.html.twig 	modified:   templates/produit/index.html.twig

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,15 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/nos-produits', name: 'app_produit')]
+    private $entityManager ;
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+
+    {
+
+
+        $this->entityManager = $entityManagerInterface;
+    }
+    
+
+
+    #[Route('/nos-produits', name: 'app_produit')] 
     public function index(): Response
     {
-        return $this->render('produit/index.html.twig');
+        $produit = $this->entityManager->getRepository(produit::class)->findAll();
+
+        return $this->render('produit/index.html.twig',[
+            'produit'=>$produit
+        ] 
+     );
+
     }
 }
