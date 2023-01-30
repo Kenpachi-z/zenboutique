ea6f0871d8bf3cf90619715bd5899e740b687ffa
KENPACHI <aaron.homassel@gmail.com>
2023-01-30 14:21:18 +0100

    modified:   public/assets/css/zenboutique.css modified:   src/.DS_Store modified:   src/Controller/ProduitController.php modified:   templates/produit/index.html.twig new file:   templates/produit/show.html.twig

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,10 +22,10 @@ class ProduitController extends AbstractController
     
 
 
-    #[Route('/nos-produits', name: 'app_produit')] 
+    #[Route('/nos-produit', name: 'app_produit')] 
     public function index(): Response
     {
-        $produit = $this->entityManager->getRepository(produit::class)->findAll();
+        $produit= $this->entityManager->getRepository(Produit::class)->findAll();
 
         return $this->render('produit/index.html.twig',[
             'produit'=>$produit
@@ -32,4 +33,22 @@ class ProduitController extends AbstractController
      );
 
     }
+
+    #[Route('/product/{Slug}', name: 'app_product')] 
+    public function show($Slug): Response
+    {
+
+        $product = $this->entityManager->getRepository(Produit::class)->findOneBy(['Slug' => $Slug]);
+        if(!$product){
+            return $this->redirectToRoute('app_produit');
+        }
+
+        return $this->render('produit/show.html.twig',[
+            'produit'=>$product
+        ] 
+     );
+
+    }
+
+
 }
