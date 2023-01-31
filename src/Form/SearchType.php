75561e1abee80d598b38357d7e13b9e370217004
KENPACHI <aaron.homassel@gmail.com>
2023-01-31 22:17:23 +0100

    modified:   src/Form/SearchType.php modified:   templates/produit/index.html.twig

--- src/Form/SearchType.php
+++ src/Form/SearchType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\classe\Search;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 class SearchType extends abstractType 
 
 {
@@ -14,13 +17,28 @@ class SearchType extends abstractType
     {
         $builder
             ->add('string', TextType::class, [
-                'label' => 'Rechercher',
+                'label' => false,
                 'required' => false,
                 'attr' => [
-                    'placeholder' => 'votre recherche...'
+                    'placeholder' => 'votre recherche...',
+                    'class'=>'form-control-sm'
                 ]
             ])
-            
+            ->add('categories', EntityType::class, [
+                'label'=> false,
+                'required'=> false,
+                'class'=>Category::class,
+                'multiple' =>true,
+                'expanded'=>true,
+
+            ] )
+            ->add('submit', SubmitType::class,
+            ['label'=>'Filtrer',
+             'attr' =>[
+                'class'=>'btn btn-dark '
+             ]
+
+            ] )
             ;
 
     }
