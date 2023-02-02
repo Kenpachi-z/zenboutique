cf9a940e65dd779a904ea87272bf97f4c12cf5d6
KENPACHI <aaron.homassel@gmail.com>
2023-02-02 21:17:11 +0100

    modified:   src/Controller/CartController.php modified:   src/classe/Cart.php modified:   templates/produit/show.html.twig

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\classe\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/mon-panier', name: 'app_cart')]
-    public function index(): Response
+    public function index(Cart $cart): Response
+
     {
+        dd($cart->get());
         return $this->render('cart/index.html.twig');
     }
     #[Route('/cart/add/{id}', name: 'app_add_to_cart')]
-    public function add($id): Response 
+    public function add(Cart $cart, $id): Response 
     {
-        dd($id);
-        return $this->render('cart/index.html.twig');
+        $cart->add($id);
+        return $this->redirectToRoute('app_cart');
     }
 
 }
--- src/classe/Cart.php
+++ src/classe/Cart.php
@@ -2,7 +2,45 @@
 
 namespace App\classe;
 
-class Cart 
+use Symfony\Component\HttpFoundation\RequestStack;
+ 
+ 
+class Cart
 {
+    private $stack;
+ 
+    public function __construct(RequestStack $stack)
+ 
+    {
+        return $this->stack = $stack;
+    }
+ 
+    public function add($id)
+    {
+ 
+        $session = $this->stack->getSession();
+        $cart = $session->get('cart', []);
+ 
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+ 
+ 
+        $session->set('cart', $cart);
+    }
+ 
+    public function get()
+    {
+        $methodget = $this->stack->getSession();
+        return $methodget->get('cart');
+    }
+ 
+    public function remove(){
+ 
+        $methodremove = $this->stack->getSession();
+        return $methodremove->remove('cart');
+    }
 
 }
